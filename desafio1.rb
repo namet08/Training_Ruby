2d51098dd14baf1b3a286cd762e6663491302d4f
Julio Santos <julio958214@gmail.com>
2022-02-21 09:18:26 -0300

    .

--- desafio1.rb
+++ desafio1.rb
@@ -1,60 +1,72 @@
 ### Desafio para calcular
-print "Olá tudo bem?
+
+loop do
+  print "Olá tudo bem?
 por favor, me fala como quer que eu te chame:"
 
-name = gets.chomp
-print "Certo, agora me fala a sua idade:"
-age = gets.chomp.to_i
+  name = gets.chomp
+  print "Certo, agora me fala a sua idade:"
+  age = gets.chomp.to_i
 
-if age >= 18
-  puts "Seja bem vindo #{name}!"
-else
-  raise "Vichi, você ainda não deve saber como usar essa ferramenta estude mais um pouco."
-end
-
-def valid_condition
-  print "Boa, então digite o primeiro numero:"
-  @number1 = gets.chomp.to_i
-  print "agora digita o segundo numero:"
-  @number2 = gets.chomp.to_i
-end
-
-def start_challenge
-  response = gets.chomp
-  if response == "sim"
-    valid_condition
-  elsif response == "bora"
-    valid_condition
-  elsif response == "Sim"
-    valid_condition
-  elsif response == "vamos"
-    valid_condition
+  if age >= 18
+    puts "Seja bem vindo #{name}!"
   else
-    raise "Há que pena, deixa pra próxima :)"
+    raise "Vichi, você ainda não deve saber como usar essa ferramenta estude, e aguarde mais um pouco."
   end
+
+  def valid_condition
+    print "Boa, então digite o primeiro numero:"
+    @number1 = gets.chomp.to_i
+    print "agora digita o segundo numero:"
+    @number2 = gets.chomp.to_i
+  end
+
+  def start_challenge
+    response = gets.chomp
+    if response == "sim"
+      valid_condition
+    elsif response == "bora"
+      valid_condition
+    elsif response == "Sim"
+      valid_condition
+    elsif response == "vamos"
+      valid_condition
+    elsif response == "vamo"
+      valid_condition
+    elsif response == "Não"
+      raise "Há que pena, deixa pra próxima :)"
+    elsif response == "não"
+      raise "Há que pena, deixa pra próxima :)"
+    elsif response == "nao"
+      raise "Há que pena, deixa pra próxima :)"
+    else
+      raise "Não entendi o que você quis dizer :s"
+    end
+  end
+
+  ### Segunda parte do desafio.
+  ## Soma
+  sleep 2
+  puts "Bora Somar?"
+  start_challenge
+  resultado = @number1 + @number2
+  puts "O valor da soma é: #{resultado}"
+
+  ## Subtração
+  puts "Agora bora subtrair?"
+  start_challenge
+  resultado = @number1 - @number2
+  puts "O valor da subtração é: #{resultado}"
+
+  ## Multiplicação
+  puts "Bora multiplicar?"
+  start_challenge
+  resultado = @number1 * @number2
+  puts "O valor da multiplicação é: #{resultado}"
+
+  ## Divisão
+  puts "Bora dividir?"
+  start_challenge
+  resultado = @number1 / @number2
+  puts "O valor da divisão é: #{resultado}"
 end
-
-### Segunda parte do desafio.
-## Soma
-puts "Bora Somar?"
-start_challenge
-resultado = @number1 + @number2
-puts "O valor da soma é: #{resultado}"
-
-## Subtração
-puts "Bora Subtrair?"
-start_challenge
-resultado = @number1 - @number2
-puts "O valor da subtração é: #{resultado}"
-
-## Multiplicação
-puts "Bora Multiplicar?"
-start_challenge
-resultado = @number1 * @number2
-puts "O valor da multiplicação é: #{resultado}"
-
-## Divisão
-puts "Bora Dividir?"
-start_challenge
-resultado = @number1 / @number2
-puts "O valor da divisão é: #{resultado}"
--- desafio4.rb
+++ desafio4.rb
@@ -1,38 +1,40 @@
 ## Multiplicando valores de um Array
 
-number = [24, 36, 48]
+array = [2, 4, 6]
 
-multiplication = number.map do |value|
+multiplication_value = array.map do |value|
   value * 2
 end
 
-puts "\nNumeros aleatorios #{number}"
-puts "\n#{multiplication} Multiplicados por 2 "
+puts "\nEsses numeros #{array}"
+sleep 2
+puts "\nMultiplicados por 2 fica #{multiplication_value}"
+sleep 2
 
 ## Imprimindo valores selecionados de uma Hash
 loop do
   puts "\n\n\Seja bem vindo a plataforma MusicoMania.\nSomente hoje temos promoções de planos gratuitos para você!"
-  puts "Vamos dar uma olhada??"
+  puts "Vamos dar uma olhada?"
   response = gets.chomp
 
   def options
     puts "Certo, esses sãos os cursos disponiveis:"
     @hash = {}
 
-    puts "1 - Curso de Guitarra"
-    puts "2 - Curso de Baixo"
-    puts "3 - Curso de Bateria"
-    puts "Qual curso você quer?"
-    cursos = gets.chomp
+    puts "Curso de Guitarra"
+    puts "Curso de Baixo"
+    puts "Curso de Bateria"
+    puts "Digite o nome do curso que você quer iniciar?"
+    curso = gets.chomp
 
-    puts "Beleza, e em qual nivel você quer começar?"
-    puts "1 - Iniciante"
-    puts "2 - Intermediario"
-    puts "3 - Avançado"
-    @hash["#{cursos}"] = gets.chomp
+    puts "Beleza, agora digite em qual nivel você quer começar."
+    puts "Iniciante"
+    puts "Intermediario"
+    puts "Avançado"
+    @hash["#{curso}"] = gets.chomp
 
-    @hash.each do |cursos, value|
-      puts "O curso escolhido foi '#{cursos}' para o nivel '#{value}' \n"
+    @hash.each do |keys, value|
+      puts "Confirmando o curso escolhido foi o '#{curso}' no nivel'#{value}' \n"
     end
   end
 
@@ -56,5 +58,6 @@ numbers = { A: 10, B: 30, C: 20, D: 25, E: 15 }
 
 numbers[:J] = 60
 
+sleep 3
 puts "O maior valor de uma hash é"
 puts numbers.values.max
